29c9f0d86a9ccb70e758cbbcd52d72d927245495
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-07-26 14:43:15 -0500

    printerqueue added

---
+++ org_chart.py
@@ -0,0 +1,48 @@
+class Node():
+    """Node in a tree."""
+
+    def __init__(self, data, children):
+        self.data = data
+        self.children = children
+
+    def __repr__(self):
+        """Reader-friendly representation."""
+
+        return f"<Node {self.data}>"
+
+    def find(self, data):
+        """Return node object with this data.
+
+        Start here. Return None if not found.
+        """
+
+        to_visit = [self]
+
+        while to_visit:
+            current = to_visit.pop()
+
+            if current.data == data:
+                return current
+
+            to_visit.extend(current.children)
+
+
+class Tree():
+    """Tree."""
+
+    def __init__(self, root):
+        self.root = root
+
+    def __repr__(self):
+        """Reader-friendly representation."""
+
+        return f"<Tree root={self.root}>"
+
+    def find_in_tree(self, data):
+        """Return node object with this data.
+
+        Start at root.
+        Return None if not found.
+        """
+
+        return self.root.find(data)
